b64ba422b9eabd7b27eb82b41ec611796d4ccea8
frolov_sd <frolov_sd@ekb.sima-land.ru>
2025-02-09 00:33:57 +0500

    init

--- main.go
+++ main.go
@@ -1,27 +1,77 @@
-package runner
+package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
 
 type TaskStorage struct {
-	TestField string
+	Storage
+	counter int8
+}
+
+func (s *TaskStorage) Counter() int8 {
+	return s.counter
+}
+
+func (s *TaskStorage) SetCounter(counter int8) {
+	s.counter = counter
+}
+
+func NewStorage() *TaskStorage {
+	return &TaskStorage{
+		Storage: &DefaultStorage{},
+	}
+}
+
+type MainInterface interface {
+	TaskMethodInterface
 }
 
 func main() {
-	New()
+	ctx := context.Background()
+	storage := NewStorage()
+	runner := New[TaskMethodInterface]().
+		Add(task1[TaskMethodInterface]()).
+		Add(task2[TaskMethodInterface]()).
+		Add(task3[TaskMethodInterface]()).
+		Run
+
+	err := runner(ctx, storage)
+	if err != nil {
+		return
+	}
 }
 
-func task1() {
-	fmt.Println(1)
+type TaskMethodInterface interface {
+	Storage
+	Counter() int8
+	SetCounter(int8)
 }
 
-func task2() {
-	fmt.Println(2)
+func task1[S TaskMethodInterface]() Task[S] {
+	return func(ctx context.Context, store S) error {
+		count := store.Counter()
+		fmt.Println("counter:", count)
+		store.SetCounter(count + 1)
+		return nil
+	}
 }
 
-func task3() {
-	fmt.Println(3)
+func task2[S TaskMethodInterface]() Task[S] {
+	return func(ctx context.Context, store S) error {
+		count := store.Counter()
+		fmt.Println("counter:", count)
+		store.SetCounter(count + 1)
+		return nil
+	}
 }
 
-func task4() {
-	fmt.Println(4)
+func task3[S TaskMethodInterface]() Task[S] {
+	return func(ctx context.Context, store S) error {
+		count := store.Counter()
+		fmt.Println("counter:", count)
+		store.SetCounter(count + 1)
+		return nil
+	}
 }
--- runner.go
+++ runner.go
@@ -1,6 +1,20 @@
-package runner
+package main
 
-import "context"
+import (
+	"context"
+)
+
+type DefaultStorage struct {
+	cancelled bool
+}
+
+func (d *DefaultStorage) Cancel() {
+	d.cancelled = true
+}
+
+func (d *DefaultStorage) IsCancelled() bool {
+	return d.cancelled
+}
 
 type Storage interface {
 	Cancel()
@@ -9,20 +23,71 @@ type Storage interface {
 
 type Task[S Storage] func(context.Context, S) error
 
-type task[S Storage] struct {
-	name         string
+type taskStruct[S Storage] struct {
 	errorHandler func(error) error
 	taskFn       Task[S]
 }
 
 type Runner[S Storage] struct {
-	tasks []task[S]
+	tasks      []taskStruct[S]
+	taskRunner func(Task[S], context.Context, S) error
 }
 
-func New[S Storage](tasks ...Task[S]) Runner[S] {
+func taskRunner[S Storage](task Task[S], ctx context.Context, storage S) error {
+	return task(ctx, storage)
+}
+
+type Option[S Storage] func(runner *Runner[S])
+
+func New[S Storage](opts ...Option[S]) Runner[S] {
 	r := Runner[S]{
-		tasks: make([]task[S], 0, len(tasks)),
+		tasks:      make([]taskStruct[S], 0, 2),
+		taskRunner: taskRunner[S],
+	}
+
+	for _, opt := range opts {
+		opt(&r)
 	}
 
 	return r
+}
+
+func (r Runner[S]) Run(ctx context.Context, storage S) error {
+	return r.linerRun(ctx, storage)
+}
+
+func (r Runner[S]) Add(task Task[S]) Runner[S] {
+	tasks := taskStruct[S]{
+		taskFn: task,
+	}
+
+	return Runner[S]{
+		tasks:      append(r.tasks, tasks),
+		taskRunner: r.taskRunner,
+	}
+}
+
+func (r Runner[S]) linerRun(ctx context.Context, storage S) error {
+	for _, entity := range r.tasks {
+		if storage.IsCancelled() {
+			break
+		}
+
+		var err error
+
+		select {
+		case <-ctx.Done():
+			err = ctx.Err()
+		default:
+			err = r.taskRunner(entity.taskFn, ctx, storage)
+		}
+
+		if entity.errorHandler != nil {
+			err = entity.errorHandler(err)
+		}
+
+		return err
+	}
+
+	return nil
 }
